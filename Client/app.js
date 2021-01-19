e10c89af9068d07b6caa20a4d628ef75cec5151b
Nathan <fourrier.nathan@outlook.fr>
2021-01-19 11:06:58 +0100

    azea

--- Client/app.js
+++ Client/app.js
@@ -5,7 +5,7 @@ var client = new ModbusRTU();
 // open connection to a tcp line
 const serverIp = "10.20.86.206";
 const port = 502;
-client.connectTCP(serverIP, { port: port });
+client.connectTCP(serverIp, { port: port });
 client.setID(1);
 
 // read the values of 10 registers starting at address 0
